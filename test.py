4dd2af59af8b4210a7ddb86b2aac69de66ac0dcc
Blackwaveee <shokri177@gmail.com>
2022-05-07 13:39:51 +0430

    Stable version

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 from AllModels import AllModels
 
-
-AllModels().reportData().randomForest(20).report().plot('confusion matrix',True).plot('roc').save('hi.txt')
+obj = AllModels()
+obj.reportData()
+obj.randomForest()
+obj.report()
+obj.plot('confusion matrix','')
+obj.plot('roc','')
+obj.save('')
+# AllModels().reportData().randomForest(0.2).report().plot('confusion matrix',True).plot('roc').save('hi.txt')
 # AllModels().reportData().knn(2).report().report().plot('confusion matrix').plot('roc')
 # AllModels().reportData().linearSVM().report().report().plot('confusion matrix').plot('roc')
 # AllModels().reportData().nonLinearSVM().report().report().plot('confusion matrix').plot('roc')
